3e6758c9d288e6c4b218a06a0e5a49cfa9c811f4
harsh shah <harsh_shah@gridle.io>
2018-11-23 14:21:47 +0530

    create view file

---
+++ src/tweets/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from .views  import tweet_detail_view,tweet_list_view,TweetDetailView,TweetListView
+
+urlpatterns = [
+    # url(r'^$', tweet_list_view,name='list'),
+    # url(r'^(?P<id>\d+)/$', tweet_detail_view,name='detail'),
+
+    url(r'^$', TweetListView.as_view(),name='list'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(),name='detail'),
+]
+
---
+++ src/tweets/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .models import Tweet
+from django.views.generic import DetailView,ListView
+# Create your views here.
+
+
+class TweetDetailView(DetailView):
+
+    template_name = 'tweets/detail_view.html'
+    queryset = Tweet.objects.all()
+    # def get_object(self):
+
+    #     id = self.kwargs.get('id')
+    #     return Tweet.objects.filter(pk=id).first()
+
+
+class TweetListView(ListView):
+
+    print('list view')
+    template_name = 'tweets/list_view.html'
+    queryset = Tweet.objects.all()
+    # print('queryset ', queryset.content)
+
+    def get_context_data(self,*args,**kwargs):
+        context = super(TweetListView,self).get_context_data(*args,**kwargs)
+        return context
+
+
+def tweet_detail_view(request,pk):
+
+    tweet = Tweet.objects.filter(pk=pk).first()
+    context = {
+        'object' : tweet,
+        # 'hello ': 'harsh'
+    } 
+    return render(request,'tweets/detail_view.html',context)
+
+def tweet_list_view(request):
+
+    tweet = Tweet.objects.filter()
+    context = {
+        'object_list' : tweet
+    } 
+    return render(request,'tweets/list_view.html',context)
